116aec9ae885587a3d852715fe3b31974a710bb2
felipecabesssa <felipesoares100@hotmail.com>
2023-01-08 21:55:42 -0300

    Bean-Validation com Mensagem customizada

--- src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
+++ src/main/java/com/devsuperior/dscatalog/resources/exceptions/StandardError.java
@@ -9,7 +9,7 @@ public class StandardError implements Serializable {
 	private Instant timestamp;
 	private Integer status;
 	private String error;
-	private String message;
+//	private String message;
 	private String path;
 	
 	public StandardError() {
@@ -39,13 +39,13 @@ public class StandardError implements Serializable {
 		this.error = error;
 	}
 
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
+//	public String getMessage() {
+//		return message;
+//	}
+//
+//	public void setMessage(String message) {
+//		this.message = message;
+//	}
 
 	public String getPath() {
 		return path;
